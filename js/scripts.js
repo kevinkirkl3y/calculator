966104d653ecca23bacd8f9d222c8cb7e214c4e6
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-10-21 14:09:51 -0700

    completed user interface logic segment
    Co-authored-by: Maxi Benbassat <info@benbassatdigital.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,20 @@
-function add (number1, number2) {
+//Business logic:
+const add = function(number1, number2) {
 	return number1 + number2;
 }
-function multiply (number1, number2) {
+const multiply = function(number1, number2) {
   return number1 * number2;
 }
-function divide (number1, number2) {
+const divide = function(number1, number2) {
   return number1 / number2;
 }
-function subtract (number1, number2) {
+const subtract = function(number1, number2) {
   return number1 - number2;
 }
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = subtract(number1, number2);
-alert(result);
+//Everything below this line is user interface logic:
+$(document).ready(function() {
+  const number1 = parseInt(prompt("Enter a number:"));
+  const number2 = parseInt(prompt("Enter another number:"));
+  const result = subtract(number1, number2);
+  alert(result);
+});  
